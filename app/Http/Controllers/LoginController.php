94536b2cb7d7b8757149a6c6fa2fd34c9560df76
JohnnyJiang94738 <68981141+JohnnyJiang94738@users.noreply.github.com>
2020-12-30 23:22:43 +0800

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,14 +13,12 @@ class LoginController extends TestController
         $member = User::where('email', $request->email)->where('password', $request->password)->first();
         $apiToken = Str::random(10); //隨機產生一組10個英數字組成的字串
         if($member)
-        {
+        {   
+            $apiToken = Str::random(10);
             if ($member->update(['api_token'=>$apiToken]))
-            { //更新 api_token
-//                if ($member->isAdmin)
-//                    return "login as admin, your api token is $apiToken";
-//                    return response()->json('Imlogin', 200);
-//                else{
-//                    return "login as user, your api token is $apiToken";
+            {
+                    $member->api_token = $apiToken;
+                    $member->save();
                     return response()->json('Imlogin', 200);
             }
         }
